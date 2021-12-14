c319eee21d8322f0494b5dce0177cc877a2a139c
Marielena <maizaga01@gmail.com>
2021-12-13 20:24:43 -0500

    Updates

--- componentes/seccionEditarPerfil.js
+++ componentes/seccionEditarPerfil.js
@@ -23,7 +23,7 @@ export const contenidoEditarPerfil = () => {
                 <li class="list">
                     <a href="#/artperfil">
                         <span class="icon">
-                            <img src="imagenes/imgUsuario.png">
+                            <img src="imagenes/ImgUsuario.png">
                         </span>                        
                     </a>
                 </li>
@@ -36,22 +36,22 @@ export const contenidoEditarPerfil = () => {
                 <div class="modalContent modalClose" id="modal">
                     <div class="cajaSuperior">
                         <div class="fondoImagenSecPerfil">
-                            <img src="imagenes/imgDelUsuario.png">
+                            <img src="imagenes/ImgDelUsuario.png">
                         </div>  
                         <div class="infActualDelUsuario" id="infActualDelUsuario">
                             <div class="imgPerfilUsuario">
-                                <img src="imagenes/imgUsuario.png">
+                                <img src="imagenes/ImgUsuario.png">
                             </div>
                     
                             <div class="contenidoTextPerfil">
-                                <h2>Lucía Lopez</h2>
-                                <p>Amante de los animales</p>
-                                <p>Lima -Perú</p>
+                                <h2 id="nombreDelPerfil"></h2>
+                                <p id="descripcionDelPerfil"></p>
+                                <p id="ubicacionDelPerfil"></p>
                             </div>
                         </div>
                     </div>
 
-                    <div class="modalFormulario" id="mmodalFormulario">
+                    <div class="modalFormulario" id="modalFormulario">
                         <form id="formIngreso">
                             <div class="cajaImputDatos">
                                 <p class="textSeccActualizacion" id="">Nombre de Usuario:</p>
@@ -85,9 +85,21 @@ export const btnEditarPerfil = () => {
     const btnGuardarCambios = document.getElementById("guardarCambios"); 
     btnGuardarCambios.addEventListener("click", (e) =>{
         e.preventDefault();
+        const inputNombreActualizado = document.getElementById("actualizacionNombre").value;
+        const inputDescripcionActualizado = document.getElementById("actualizacionEstado").value;
+        const inputUbicacionActualizado = document.getElementById("actualizacionUbicacion").value;
         const userData = JSON.parse(sessionStorage.userSession);
-        actualizarPerfil(userData.id);
-
+        actualizarPerfil(userData.id, inputNombreActualizado, inputUbicacionActualizado, inputDescripcionActualizado);
     });
     
 };
+
+export const actualizarDatosPerfil = (name, ubicacion, descripcion) => {
+    const nombreDelPerfil = document.getElementById("nombreDelPerfil"); 
+    const ubicacionDelPerfil = document.getElementById("ubicacionDelPerfil"); 
+    const descripcionDelPerfil = document.getElementById("descripcionDelPerfil"); 
+    nombreDelPerfil.innerHTML = name;
+    ubicacionDelPerfil.innerHTML = ubicacion;
+    descripcionDelPerfil.innerHTML = descripcion;
+} 
+
